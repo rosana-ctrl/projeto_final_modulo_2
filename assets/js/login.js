57e3f4ec6fc22f7aecfdbeadc4889f71af22a31a
lisandradecassia <lisandradecassia@gmail.com>
2022-05-26 10:58:20 -0300

    Realizei ajuste de responsividade na pagina de recuperar senha.

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,15 +1,4 @@
-/*onclick para direcionar para página de cadastro e recuperação de senha*/
-function cadastro() {
-    location.assign("../cadastro.html");
-}
-function recuperarSenha() {
-    location.assign("./recuperarSenha.html");
-}
-function home() {
-    location.assign("./recuperarSenha.html");
-}
-
-/* Poupap ao solicitar recuperção de senha*/
+/* Poupap ao solicitar recuperação de senha*/
 const btn = document.querySelector('#btn')
 const popup = document.querySelector('#pop-wrapper')
 
